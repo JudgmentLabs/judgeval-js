cfa567d04d1a683777f2878fbe0aaca9528b0019
Abhishek Govindarasu <abhishekgovindarasu@gmail.com>
2026-02-02 14:07:41 -0800

    chore: remove context keys

--- src/tracer/processors/_lifecycles/contextKeys.ts
+++
@@ -1,9 +0,0 @@
-import { createContextKey } from "@opentelemetry/api";
-import { AttributeKeys } from "../../../judgmentAttributeKeys";
-
-export const CUSTOMER_ID_KEY = createContextKey(
-  AttributeKeys.JUDGMENT_CUSTOMER_ID,
-);
-export const SESSION_ID_KEY = createContextKey(
-  AttributeKeys.JUDGMENT_SESSION_ID,
-);
--- src/tracer/processors/_lifecycles/index.ts
+++ src/tracer/processors/_lifecycles/index.ts
@@ -1,7 +1,6 @@
 import "./CustomerIdProcessor";
 import "./SessionIdProcessor";
 
-export { CUSTOMER_ID_KEY, SESSION_ID_KEY } from "./contextKeys";
 export { CustomerIdProcessor } from "./CustomerIdProcessor";
-export { getAll, register } from "./registry";
 export { SessionIdProcessor } from "./SessionIdProcessor";
+export { getAll, register } from "./registry";
